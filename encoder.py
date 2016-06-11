15a613c59d954be6f30b404e3146f088a3b9c78c
David Case <Dav3xor@gmail.com>
2016-06-11 14:58:08 -0700

    added docstrings

--- encoder.py
+++ encoder.py
@@ -1,5 +1,10 @@
 
+
+
 def encode(filename, stream):
+    """
+    encode <filename into a bytearray <stream>
+    """
     try:
         with open(filename, 'rb') as infile:
             input_file = infile.read()
@@ -13,6 +18,10 @@ def encode(filename, stream):
 # files larger than memory could be supported, but will currently
 # not work.  More than happy to add that feature if it comes up.
 def decode(stream, filename):
+    """
+    decode the first file found in <stream>
+    and write it to <filename>.
+    """
     if len(stream):
         # wire protocol is in the form of <length>|file<length>|file....<length>|file
         file_len, contents = stream.split('|',1)
